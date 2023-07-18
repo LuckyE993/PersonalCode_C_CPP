1c5ca433710d29ecd84a56a97ab33d9939c1b365
Lucky_E993 <luckyeureka52@gmail.com>
2023-07-18 17:30:18 +0800

    801. 二进制中1的个数

---
+++ acwing/801.cpp
@@ -0,0 +1,24 @@
+﻿//
+// Created by Lucky_E on 2023-07-18.
+//
+#include <iostream>
+using namespace std;
+
+int num,x;
+
+int main()
+{
+    cin >> num;
+    while(num--)
+    {
+        int count = 0;
+        cin>>x;
+        while(x)
+        {
+            count++;
+            x = x&(x-1);
+        }
+        cout << count << " ";
+    }
+    return 0;
+}
