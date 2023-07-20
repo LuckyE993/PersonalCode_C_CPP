d7ed0078d01ab25957044ac88678a13d0f0a164b
Lucky_E993 <luckyeureka52@gmail.com>
2023-07-20 16:06:15 +0800

    stl_learn_list.cpp

---
+++ C_Primer_Plus_Learn/STL/stl_learn_list.cpp
@@ -0,0 +1,67 @@
+﻿// list_splice.cpp
+// compile with: /EHsc /W4
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+template <typename S> void print(const S& s) {
+    cout << s.size() << " elements: ";
+
+    for (const auto& p : s) {
+        cout << "(" << p << ") ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    list<int> c1{10,11};
+    list<int> c2{20,21,22};
+    list<int> c3{30,31};
+    list<int> c4{40,41,42,43};
+
+    list<int>::iterator where_iter;
+    list<int>::iterator first_iter;
+    list<int>::iterator last_iter;
+
+    cout << "Beginning state of lists:" << endl;
+    cout << "c1 = ";
+    print(c1);
+    cout << "c2 = ";
+    print(c2);
+    cout << "c3 = ";
+    print(c3);
+    cout << "c4 = ";
+    print(c4);
+
+    where_iter = c2.begin();
+    ++where_iter; // start at second element
+    c2.splice(where_iter, c1);
+    cout << "After splicing c1 into c2:" << endl;
+    cout << "c1 = ";
+    print(c1);
+    cout << "c2 = ";
+    print(c2);
+
+    first_iter = c3.begin();
+    c2.splice(where_iter, c3, first_iter);
+    cout << "After splicing the first element of c3 into c2:" << endl;
+    cout << "c3 = ";
+    print(c3);
+    cout << "c2 = ";
+    print(c2);
+
+    first_iter = c4.begin();
+    last_iter = c4.end();
+    // set up to get the middle elements
+    ++first_iter;
+    --last_iter;
+    c2.splice(where_iter, c4, first_iter, last_iter);
+    cout << "After splicing a range of c4 into c2:" << endl;
+    cout << "c4 = ";
+    print(c4);
+    cout << "c2 = ";
+    print(c2);
+}
