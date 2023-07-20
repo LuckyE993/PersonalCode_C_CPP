3965618aee01aa58af7f88f307c5a4813223faa0
Lucky_E993 <luckyeureka52@gmail.com>
2023-07-20 10:28:06 +0800

    stl_learn_stack.cpp

---
+++ C_Primer_Plus_Learn/STL/stl_learn_stack.cpp
@@ -0,0 +1,26 @@
+﻿//
+// Created by Lucky_E on 2023-07-20.
+//
+#include "iostream"
+#include "stack"
+using namespace std;
+int main()
+{
+    stack<int> stack1;
+    for(int i = 0; i < 10; i++)
+    {
+        stack1.push(i);
+        cout << "The stack top is " << stack1.top()<<endl;
+    }
+    stack1.pop();
+    cout<<"================================================================"<<endl;
+    cout << "The stack top is " << stack1.top()<<" after pop"<<endl;
+    cout<<"================================================================"<<endl;
+    while(!stack1.empty())
+    {
+        cout << stack1.top()<<" ";
+        stack1.pop();
+    }
+
+    return 0;
+}
