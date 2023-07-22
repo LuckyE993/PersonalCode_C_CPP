98926e31d4270e8a22f46f0a0f3cde4aaf7fa2cd
Lucky_E993 <luckyeureka52@gmail.com>
2023-07-22 18:42:18 +0800

    842_STL.cpp

---
+++ acwing/842_STL.cpp
@@ -0,0 +1,43 @@
+﻿//
+// Created by Lucky_E on 2023-07-22.
+//
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+int n;
+void DFS(vector<int>& Stack,vector<bool>& Used,int num)
+{
+    if(Stack.size() == n)
+    {
+        for(auto number : Stack)
+        {
+            cout<<number<<" ";
+        }
+        cout<<endl;
+    }
+
+    for(int i=1;i<=num;i++)
+    {
+        if(!Used[i])
+        {
+            Used[i] = true;
+            Stack.push_back(i);
+            DFS(Stack,Used,num);
+            Stack.pop_back();
+            Used[i] = false;
+        }
+    }
+}
+
+int main()
+{
+
+    cin >> n;
+    vector<bool> used(n+1,false);
+    vector<int> Stack;
+
+    DFS(Stack,used,n);
+
+    return 0;
+}
