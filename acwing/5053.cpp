5b20a7ad9a6bcaf2ebda1b74f6e764e4803853d9
Lucky_E993 <luckyeureka52@gmail.com>
2023-07-22 19:14:25 +0800

    周赛113T1

---
+++ acwing/5053.cpp
@@ -0,0 +1,27 @@
+﻿//
+// Created by Lucky_E on 2023-07-22.
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+    int count;
+    cin>>count;
+    while (count--)
+    {
+        int n;
+        cin>>n;
+        int Mod = n/3;
+        while(Mod--)
+        {
+            cout<<"abc";
+        }
+        char c[3]={'a', 'b', 'c'};
+        for(int i=0;i<n%3;i++)
+        {
+            cout<<c[i];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
