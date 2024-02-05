2c73f824e7c252f88050fe74bc127554d2d84a98
Lucky_Eureka <90081491+LuckyE993@users.noreply.github.com>
2024-02-05 16:41:34 +0800

    Algorithm TrieTree cpp ver.

--- Algorithm/CLASS/TrieTree.cpp
+++ Algorithm/CLASS/TrieTree.cpp
@@ -2,7 +2,87 @@
 // Created by LuckyE on 2024/2/4.
 //
 #include "bits/stdc++.h"
+#define Cpp 1
+#if Cpp
+using namespace std;
+const int N=100010;
+struct TrieNode
+{
+	int cnt;
+	TrieNode* next[26]{};
 
+	TrieNode()
+	{
+		cnt = 0;
+		memset(next,0, sizeof(next));
+	}
+
+};
+
+class Trie
+{
+public:
+	Trie()
+	{
+		root = new TrieNode;
+	}
+	void insert(const string& word)
+	{
+		TrieNode*  cur = root;
+		for(char c: word)
+		{
+			int index = c-'a';
+			if(cur->next[index]== nullptr)
+			{
+				cur->next[index] = new TrieNode();
+
+			}
+			cur = cur->next[index];
+		}
+		cur->cnt++;
+	}
+
+	int query(const string& word)
+	{
+		TrieNode* cur = root;
+		for(char c : word)
+		{
+			int index = c-'a';
+			if(cur->next[index]== nullptr)
+			{
+				return 0;
+			}
+			cur=cur->next[index];
+		}
+		return cur->cnt;
+	}
+private:
+	TrieNode* root;
+};
+int main()
+{
+	Trie trie;
+	int n;
+	cin>>n;
+	while(n--)
+	{
+		string op,word;
+		cin>>op;
+		cin>>word;
+		if(op == "I")
+		{
+			trie.insert(word);
+		}
+		if(op=="Q")
+		{
+			cout<<trie.query(word)<<endl;
+		}
+	}
+	return 0;
+}
+
+
+#else
 using namespace std;
 const int N=100010;
 int son[N][26],cnt[N],idx;
@@ -59,3 +139,5 @@ int main()
 
 	return 0;
 }
+
+#endif
