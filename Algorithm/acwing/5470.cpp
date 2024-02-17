58051a641cee7b6fcbca80b11245175884a399b4
Lucky_Eureka <90081491+LuckyE993@users.noreply.github.com>
2024-02-17 23:48:42 +0800

    ACWing 5470 2024年2月17日

---
+++ Algorithm/acwing/5470.cpp
@@ -0,0 +1,15 @@
+//
+// Created by LuckyE on 2024/2/17.
+//
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if(n%12==0)
+		cout<<12<<endl;
+	else
+	cout<<n%12<<endl;
+	return 0;
+}
