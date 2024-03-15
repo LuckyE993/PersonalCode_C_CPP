34c14d75a998c9066590bfd27672c1e26d25342e
Lucky_Eureka <90081491+LuckyE993@users.noreply.github.com>
2024-03-15 11:29:29 +0800

    ACWing 2024年3月15日 二分 789.数的范围

--- Algorithm/acwing/789.cpp
+++ Algorithm/acwing/789.cpp
@@ -29,21 +29,55 @@ int SR(int l,int r ,int x)
 
 }
 
+int solution2_left(int x)
+{
+	int l=-1,r=n;
+	while(l+1!=r)
+	{
+		int mid = l+r >> 1;
+		if(a[mid]>=x)
+			r = mid;
+		else
+			l = mid;
+	}
+	if(a[r]!=x)
+		return -1;
+	else
+		return r;
+}
+int solution2_right(int x)
+{
+	int l = -1,r = n;
+	while(l+1 != r)
+	{
+		int mid = l+r >>1;
+		if(a[mid] <= x)
+			l = mid;
+		else
+			r = mid;
+	}
+	if(a[l]!=x)
+		return -1;
+	else
+		return l;
+}
 int main()
 {
-	cin>>n>>q;
+	scanf("%d %d",&n,&q);
 	for(int i=0;i<n;i++)
-		cin>>a[i];
+		scanf("%d",&a[i]);
 	while(q--)
 	{
-		cin>>k;
-		int l=SL(0,n-1,k);
-		if(a[l]!=k)
-			cout<<"-1 -1"<<endl;
-		else
-		{
-			cout<<l<<" "<<SR(0,n-1,k)<<endl;
-		}
+//		cin>>k;
+//		int l=SL(0,n-1,k);
+//		if(a[l]!=k)
+//			cout<<"-1 -1"<<endl;
+//		else
+//		{
+//			cout<<l<<" "<<SR(0,n-1,k)<<endl;
+//		}
+		scanf("%d",&k);
+		printf("%d %d\n", solution2_left(k), solution2_right(k));
 	}
 	return 0;
 }
