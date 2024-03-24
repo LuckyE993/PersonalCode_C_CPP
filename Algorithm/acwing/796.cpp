0ac1270f390af170013567aab002c139647f5741
Lucky_Eureka <90081491+LuckyE993@users.noreply.github.com>
2024-03-24 11:29:13 +0800

    ACWing 2024年3月24日 2D前缀和 796

--- Algorithm/acwing/796.cpp
+++ Algorithm/acwing/796.cpp
@@ -1,30 +1,23 @@
-#include "iostream"
-using namespace std;
+#include <cstdio>
 const int N = 1010;
 int a[N][N],s[N][N];
-
+int n,m,q;
 int main()
 {
-    int n,m,q;
-
-    cin >> n >> m >> q;
-
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=m;j++)
-        {
-            cin >>a[i][j];
-            s[i][j] = a[i][j]+s[i][j-1]-s[i-1][j-1]+s[i-1][j];
-        }
-    }
-
-    while(q--)
-    {
-        int x1,y1,x2,y2;
-        cin>>x1>>y1>>x2>>y2;
-        cout<<s[x2][y2]-s[x1-1][y2]-s[x2][y1-1]+s[x1-1][y1-1]<<endl;
-
-    }
-
-    return 0;
+	scanf("%d %d %d",&n,&m,&q);
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=m;j++)
+		{
+			scanf("%d",&a[i][j]);
+			s[i][j] = a[i][j]+s[i-1][j]+s[i][j-1]-s[i-1][j-1];
+		}
+	}
+	while(q--)
+	{
+		int x1,x2,y1,y2;
+		scanf("%d %d %d %d ",&x1,&y1,&x2,&y2);
+		printf("%d \n",s[x2][y2]-s[x1-1][y2]-s[x2][y1-1]+s[x1-1][y1-1]);
+	}
+	return 0;
 }
