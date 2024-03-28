bdec7922ba3f99dad435b7e8fc16105db14285b0
Lucky_Eureka <90081491+LuckyE993@users.noreply.github.com>
2024-03-28 21:43:11 +0800

    ACWing 2024年3月27日 数论 1216. 饮料换购

---
+++ Algorithm/acwing/1216.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int ans = 0;
+	int n;
+	scanf("%d",&n);
+	int temp = n;
+	while(n>=3)
+	{
+		n-=2;
+		ans++;
+	}
+	printf("%d",ans+temp);
+	return 0;
+}
