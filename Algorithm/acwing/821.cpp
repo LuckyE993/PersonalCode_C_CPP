19a8033c52834e293dbdc657a455f26c40b87ba8
Lucky_Eureka <90081491+LuckyE993@users.noreply.github.com>
2024-04-09 11:58:36 +0800

    ACWing  2024年4月9日 DFS 记忆化搜索 DP 821.跳台阶

--- Algorithm/acwing/821.cpp
+++ Algorithm/acwing/821.cpp
@@ -2,8 +2,17 @@
 // Created by LuckyE on 2024/2/20.
 //
 #include <bits/stdc++.h>
+#include "cstring"
 using namespace std;
+
+#define DFS_Mem false
+#define DFS false
+#define DP true
+
+const int N =17;
 int n;
+
+//Solution begin
 int Fibnacci(int x)
 {
 	if(x==1)
@@ -14,14 +23,66 @@ int Fibnacci(int x)
 	{
 		return 1;
 	}
-
-
 	return Fibnacci(x-1)+ Fibnacci(x-2);
 }
+
+
+int mem[N];
+int dfs_with_mem(int x)
+{
+	if(mem[x])
+	{
+		return mem[x];
+	}
+
+	int sum = 0 ;
+
+	if(x==1)
+		sum = 1;
+	else if(x==2)
+		sum = 2;
+	else
+		sum = dfs_with_mem(x-1)+ dfs_with_mem(x-2);
+
+	mem[x] = sum;
+	return sum;
+}
+
+
+int a[N];
+int dp(int x)
+{
+	if(x == 1)
+		return 1;
+	else if(x == 2)
+		return 2;
+	else
+	{
+		a[1] = 1;
+		a[2] = 2;
+		for(int i=3;i<=x;i++)
+		{
+			a[i] = a[i-1]+a[i-2];
+		}
+		return a[x];
+	}
+}
+
 
 int main()
 {
 	cin>>n;
-	cout << Fibnacci(n);
+	if(DFS)
+		cout << Fibnacci(n);
+	if(DFS_Mem)
+	{
+		memset(mem,0,sizeof mem);
+		cout<<dfs_with_mem(n);
+	}
+	if(DP)
+	{
+		cout<<dp(n);
+	}
+
 	return 0;
 }
