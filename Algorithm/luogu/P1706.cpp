6556abd3b81d37dc419f8e27624c589570c2f4a2
Lucky_Eureka <90081491+LuckyE993@users.noreply.github.com>
2024-08-23 17:54:59 +0800

    Luogu DFS 2024年8月23日

--- Algorithm/luogu/P1706.cpp
+++ Algorithm/luogu/P1706.cpp
@@ -2,40 +2,40 @@
 // Created by LuckyE on 2024/2/20.
 //
 #include "iostream"
+#include "iomanip"
+
 using namespace std;
 const int N=1e5+10;
 int n;
 bool st[N];
-int arr[N];
+int out[N];
 
 void dfs(int x)
 {
 	if(x>n)
 	{
 		for(int i=1;i<=n;i++)
-		{
-			cout<<"    "<<arr[i];
-		}
+			cout<<setw(5)<<out[i];
 		cout<<endl;
-		return;
 	}
-
 
 	for(int i=1;i<=n;i++)
 	{
 		if(!st[i])
 		{
 			st[i] = true;
-			arr[x] = i;
+			out[x] = i;
 			dfs(x+1);
-			st[i]= false;
-			arr[x] = 0;
+			out[x] = 0;
+			st[i] = false;
 		}
 	}
+
 }
+
 int main()
 {
-	cin>>n;
+	cin >> n;
 	dfs(1);
 	return 0;
 }
